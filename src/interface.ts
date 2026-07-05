bf6d41f0ddce5f5161ac8d2794896d194ef57475
AritraApon <aritromazumdar8@gmail.com>
2026-07-06 00:23:54 +0600

    oop implemnets

--- src/interface.ts
+++ src/interface.ts
@@ -1,4 +1,4 @@
-//! Interface and optional (?) | readOnly |literal Types | Intersection
+//*Interface and optional (?) | readOnly |literal Types | Intersection |implements
 
 //todo problem - 01 Interface and optional (?) | readOnly
 interface Doctor {
@@ -34,5 +34,32 @@ type BaseTicket = {
 
 type tickerDetails = {
     ticketName:string,
+     setTime: string,
+}
+
+type FullTicketInfo = BaseTicket & tickerDetails ;
+
+let ticket:FullTicketInfo ={
+   id:'1234',
+   price:220,
+   ticketName:'Dhaka Express',
+   setTime: '10 july , 2026 at 5pm',
 
 }
+
+// console.log(ticket)
+
+//todo problem - 04 OOP- implements
+
+interface NotificationSender {
+    send(message: string): void;
+}
+
+class SMSNotification implements NotificationSender{
+    send(message: string): void {
+        console.log(`Sending ${message}`)
+    }
+}
+
+const smsApp = new SMSNotification()
+smsApp.send("Your ticket booking is confirmed! ")
